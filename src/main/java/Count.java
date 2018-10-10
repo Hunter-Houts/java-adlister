e4d68d9b7f507fc943409e7dc0daad4d4595d648
Hunter-Houts <hunter.alexander.houts@gmail.com>
2018-10-10 11:06:38 -0500

    Count exercise complete

---
+++ src/main/java/Count.java
@@ -0,0 +1,17 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(name="Adlister Server", urlPatterns = "/count")
+public class Count extends HttpServlet {
+    public static int count = 0;
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        ++count;
+        PrintWriter out = res.getWriter();
+        out.println(count);
+
+    }
+
+}
